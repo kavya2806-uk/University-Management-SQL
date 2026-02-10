67d179e34d188ba6199315c929e4ce21d05c734f
kayva <kavyanathan2806@gmail.com>
2026-02-10 19:13:31 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,28 @@
+INSERT INTO Departments VALUES
+(1, 'CSE'),
+(2, 'IT'),
+(3, 'ECE'),
+(4, 'AIDS');
+
+INSERT INTO Students VALUES
+(101, 'Kavya', 1, 2),
+(102, 'Riya', 2, 2),
+(103, 'Anu', 1, 1),
+(104, 'Meena', 3, 3);
+
+INSERT INTO Courses VALUES
+(1, 'SQL'),
+(2, 'Python'),
+(3, 'Data Science');
+
+INSERT INTO Teachers VALUES
+(201, 'Dr. Priya', 1),
+(202, 'Mr. Arun', 2),
+(203, 'Dr. Kumar', 3);
+
+INSERT INTO Marks VALUES
+(101, 1, 95),
+(101, 2, 88),
+(102, 1, 85),
+(103, 3, 90),
+(104, 2, 92);
